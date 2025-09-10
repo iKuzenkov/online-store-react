2200e0d62d28cab0205f61354fea08a8dac5d190
Igor <greendaycom1@gmail.com>
2025-09-10 23:12:44 +0300

    upd: App.jsx, added new path /:id

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import ProductsPage from "../pages/productsPage/ProductsPage";
+import ProductDetails from "../pages/productDetails/ProductDetails";
 import Catalog from "../pages/Catalog";
 import Cart from "../pages/cart/Cart";
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />} />
             <Route path="products" element={<ProductsPage />} />
+            <Route path="products/:id" element={<ProductDetails />} />
             <Route path="catalog" element={<Catalog />} />
             <Route path="cart" element={<Cart />} />
           </Route>
