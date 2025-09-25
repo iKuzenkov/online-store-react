48f2d46cedafec0ba26e3e4391a3277f7f89c77a
Igor <greendaycom1@gmail.com>
2025-09-26 00:40:09 +0300

    added massege about mistake, adminpanel

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -16,6 +16,11 @@ function AdminPage() {
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [image, setImage] = useState("");
+  const [errors, setErrors] = useState({
+    name: "",
+    price: "",
+    category: "",
+  });
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -31,10 +36,16 @@ function AdminPage() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    const numericPrice = Number(price);
+    if (numericPrice <= 0 || isNaN(numericPrice)) {
+      alert("Price must be greater then 0");
+      return;
+    }
+
     const newProduct = {
       id: Date.now(),
       name,
-      price: Number(price),
+      price: numericPrice,
       category,
       image: image,
     };
@@ -52,38 +63,74 @@ function AdminPage() {
       <div className="admin-panel">
         <h1>Admin Panel</h1>
         <form className="admin-form" onSubmit={handleSubmit}>
-          <label>
+          <label htmlFor="name">
             Name:{" "}
             <input
               type="text"
               value={name}
+              id="name"
               required
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e) => {
+                const value = e.target.value;
+                setName(value);
+
+                setErrors((prev) => ({
+                  ...prev,
+                  name: value === "" ? "Please, enter name." : "",
+                }));
+              }}
             />
           </label>
+          {errors.name && <div className="error">{errors.name}</div>}
           <label>
             Price:{" "}
             <input
               type="number"
               value={price}
+              min="1"
+              name="price"
               required
-              onChange={(e) => setPrice(e.target.value)}
+              onChange={(e) => {
+                const value = e.target.value;
+                setPrice(value);
+
+                setErrors((prev) => ({
+                  ...prev,
+                  price: value === "" ? "Please, enter price." : "",
+                }));
+              }}
             />
           </label>
+          {errors.price && <div className="error">{errors.price}</div>}
           <label>
             Category:{" "}
             <select
               value={category}
-              onChange={(e) => setCategory(e.target.value)}
+              onChange={(e) => {
+                const value = e.target.value;
+                setCategory(value);
+
+                setErrors((prev) => ({
+                  ...prev,
+                  category: value === "" ? "Please, choose category" : "",
+                }));
+              }}
             >
+              <option value="">Choose category</option>
               <option value="books">Books</option>
               <option value="electronics">Electronics</option>
               <option value="clothes">Clothes</option>
             </select>
           </label>
+          {errors.category && <div className="error">{errors.category}</div>}
           <label>
             Image:{" "}
-            <input type="file" accept="image/*" onChange={handleImageChange} />
+            <input
+              type="file"
+              accept="image/*"
+              required
+              onChange={handleImageChange}
+            />
           </label>
           {image && (
             <div className="preview">
